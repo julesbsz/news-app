375faab3275b5ba688de3c78d28395af69a2d295
Jules <j.bousrez@outlook.com>
2024-01-26 09:27:51 +0100

    feat: added saved articles page

--- app/(inside)/savedArticles.jsx
+++ app/(inside)/savedArticles.jsx
@@ -1,14 +1,58 @@
-import { StyleSheet, Text, SafeAreaView } from "react-native";
-import React from "react";
+import { StyleSheet, Text, SafeAreaView, FlatList } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import NewsCardComponent from "../components/NewsCard";
 
 const SavedArticlesPage = () => {
-	return (
-		<SafeAreaView>
-			<Text>SavedArticlesPage</Text>
-		</SafeAreaView>
-	);
+	const router = useRouter();
+	const [savedArticles, setSavedArticles] = useState([]);
+
+	const getData = async () => {
+		try {
+			const savedArticles = await AsyncStorage.getItem("saved-articles");
+			if (savedArticles !== null) {
+				setSavedArticles(JSON.parse(savedArticles));
+			}
+		} catch (e) {
+			displayAlert("Error", "There was an error getting your saved articles.");
+		}
+	};
+
+	const displayAlert = (title, message) => {
+		Alert.alert(title, message, [{ text: "OK" }], { cancelable: false });
+	};
+
+	useEffect(() => {
+		getData();
+		console.log(savedArticles);
+
+		if (savedArticles.length === 0) {
+			console.log("empty");
+		} else {
+			console.log("not empty");
+		}
+	}, [savedArticles]);
+
+	return <SafeAreaView style={styles.container}>{savedArticles.length === 0 ? <Text style={styles.body}>You have no saved articles.</Text> : <FlatList scrollEnabled={true} numColumns={1} data={savedArticles} renderItem={({ item }) => <NewsCardComponent data={item} />} keyExtractor={(item) => item.url} />}</SafeAreaView>;
 };
 
 export default SavedArticlesPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		height: "100%",
+		width: "100%",
+		backgroundColor: "#111111",
+		display: "flex",
+	},
+	body: {
+		fontSize: 20,
+		fontWeight: "200",
+		paddingTop: 50,
+		lineHeight: 30,
+		marginRight: 20,
+		color: "#FFF2C5",
+		textAlign: "center",
+	},
+});
