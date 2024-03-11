1b5b726578bff76c5bcb2cc1baad273ecedeb40c
julesbsz <j.bousrez@outlook.com>
2024-03-11 08:07:41 +0100

    fix: camera is now properly working

--- app/(inside)/camera.jsx
+++ app/(inside)/camera.jsx
@@ -1,23 +1,63 @@
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Camera, CameraType } from "expo-camera";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const CameraPage = () => {
 	const [type, setType] = useState(CameraType.back);
-	const [permission, requestPermission] = Camera.useCameraPermissions();
+	const [hasPermission, setHasPermission] = useState(null);
+	const [cameraRef, setCameraRef] = useState(null);
 
 	function toggleCameraType() {
 		setType((current) => (current === CameraType.back ? CameraType.front : CameraType.back));
 	}
 
+	useEffect(() => {
+		(async () => {
+			const { status } = await Camera.requestCameraPermissionsAsync();
+			setHasPermission(status === "granted");
+		})();
+	}, []);
+
+	const takePicture = async () => {
+		if (cameraRef) {
+			const photo = await cameraRef.takePictureAsync();
+
+			if (photo.uri === undefined) {
+				return;
+			}
+
+			try {
+				await AsyncStorage.setItem("picture", photo.uri);
+			} catch (error) {
+				console.error(error);
+			}
+
+			navigation.navigate("Settings");
+		}
+	};
+
+	if (hasPermission === null) {
+		return <View />;
+	}
+	if (hasPermission === false) {
+		return <Text>No access to camera</Text>;
+	}
+
 	return (
 		<View style={styles.container}>
-			<Camera style={styles.camera} type={type} flashMode={true}>
-				<View style={styles.buttonContainer}>
-					<TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-						<MaterialIcons name="flip-camera-ios" size={32} color="#FFF2C5" />
-					</TouchableOpacity>
+			<Camera style={styles.camera} type={type} ref={(ref) => setCameraRef(ref)}>
+				<View>
+					<View>
+						<TouchableOpacity style={styles.buttonFlip} onPress={toggleCameraType}>
+							<MaterialIcons name="flip-camera-ios" size={32} color="#FFF2C5" />
+						</TouchableOpacity>
+					</View>
+					<View>
+						<TouchableOpacity style={styles.button} onPress={takePicture}>
+							<MaterialIcons name="radio-button-on" size={60} color="white" />
+						</TouchableOpacity>
+					</View>
 				</View>
 			</Camera>
 		</View>
@@ -25,27 +65,23 @@ const CameraPage = () => {
 };
 
 const styles = StyleSheet.create({
+	button: {
+		position: "absolute",
+		top: 520,
+		left: "50%",
+		marginLeft: -30,
+	},
+	buttonFlip: {
+		position: "absolute",
+		top: 540,
+		marginLeft: 20,
+	},
 	container: {
 		flex: 1,
+		marginBottom: 125,
 	},
 	camera: {
 		flex: 1,
 	},
-	buttonContainer: {
-		flex: 1,
-		backgroundColor: "transparent",
-		flexDirection: "row",
-		margin: 50,
-	},
-	button: {
-		flex: 0.1,
-		alignSelf: "flex-end",
-		alignItems: "center",
-	},
-	text: {
-		fontSize: 18,
-		color: "white",
-	},
 });
-
 export default CameraPage;
